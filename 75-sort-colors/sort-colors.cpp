c98d2e767decd60f72bac1cfbb1ecfe3ef5d8a61
Jeevan h m <jeevanhm72@gmail.com>
2026-09-23 01:52:17 +0530

    Time: 4 ms (0.88%) | Memory: 11.6 MB (49.13%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,35 @@
+int fn(vector<int>& arr,int low,int high){
+    int pivot=arr[low];
+    int i=low,j=high;
+    while(i<j){
+        while(arr[i]<=pivot&&i<=high-1){
+            i++;
+        }
+        while(arr[j]>pivot&&j>=low+1){
+            j--;
+        }
+        if(i<j){
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+void swap(vector<int>& a, vector<int>& b){
+    vector<int>temp=a;
+    a=b;
+    b=temp;
+}
+void qs(vector<int>& nums,int low,int high){
+        if(low<high){
+            int partition=fn(nums,low,high);
+            qs(nums,low, partition-1);
+            qs(nums,partition+1,high);
+        }
+}
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        qs(nums,0,nums.size()-1);          
+    }
+};
